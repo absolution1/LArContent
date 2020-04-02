d31fffd52a68e7e7c21d62f458b5dfd9708a01a7
Dom Brailsford <dominicbrailsford@gmail.com>
2020-04-02 05:20:42 -0500

    Missed one bit that needed commenting out

--- larpandoracontent/LArThreeDReco/LArThreeDBase/TwoViewMatchingAlgorithm.cc
+++ larpandoracontent/LArThreeDReco/LArThreeDBase/TwoViewMatchingAlgorithm.cc
@@ -194,7 +194,7 @@ void TwoViewMatchingAlgorithm<T>::PerformMainLoop()
         for (const Cluster *const pCluster2 : clusterVector2)
             this->CalculateOverlapResult(pCluster1, pCluster2);
     }
-    const pandora::Pandora * primary_pandora = MultiPandoraApi::GetPrimaryPandoraInstance(&(this->GetPandora()));
+    //const pandora::Pandora * primary_pandora = MultiPandoraApi::GetPrimaryPandoraInstance(&(this->GetPandora()));
     //PANDORA_MONITORING_API(Create(*primary_pandora));
     //PANDORA_MONITORING_API(SaveTree(*primary_pandora, "matchtree", "output.root", "RECREATE"));
     //PANDORA_MONITORING_API(Delete(*primary_pandora));
